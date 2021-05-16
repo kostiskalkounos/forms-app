13c9c7cfd45bc13b3767c1b2498b8daf6fda08ae
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-16 18:38:00 +0200

    validate input using useRef to read the value when needed

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,21 @@
-const SimpleInput = (props) => {
+import { useRef } from "react";
+
+const SimpleInput = () => {
+  const nameInputRef = useRef();
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    const enteredValue = nameInputRef.current.value;
+
+    console.log(enteredValue);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input ref={nameInputRef} type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
